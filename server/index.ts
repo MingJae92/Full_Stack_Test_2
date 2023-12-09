696deb92a2d5b3da363119240e640c2ceca1e83a
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-12-09 22:04:26 +0000

    Contributor data file path updated

--- server/index.ts
+++ server/index.ts
@@ -6,30 +6,37 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 4501;
+const port = 4502;
 
 // Uncomment this line if you need to serve static files from a directory
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // Uncomment these lines if you want to use a JSON file
-const filePath = new URL('file://' + path.join(__dirname, 'data', 'contributors.json')).pathname;
+const filePath = new URL('file://' + path.resolve(__dirname, '../data/contributor.json')).pathname;
 
-// Use import instead of require
 import { readFileSync } from 'fs';
-const contributorsRaw = readFileSync(filePath, 'utf-8');
-const contributors = JSON.parse(contributorsRaw);
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, this is your Express server!');
-});
+try {
+  const contributorsRaw = readFileSync(filePath, 'utf-8');
+  const contributors = JSON.parse(contributorsRaw);
 
-// Uncomment this route if you want to send JSON data
-app.get('/contributors', (req, res) => {
-  res.json(contributors);
-});
+  // Log the parsed JSON data to the terminal
+  console.log('Parsed JSON data:', contributors);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`App listening on http://localhost:${port}`);
-});
+  // Define a simple route
+  app.get('/', (req, res) => {
+    res.send('Hello, this is your Express server!');
+  });
+
+  // Uncomment this route if you want to send JSON data
+  app.get('/contributors', (req, res) => {
+    res.json(contributors);
+  });
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
+  });
+} catch (error) {
+  console.error('Error reading or parsing contributors.json:', (error as Error).message);
+}
