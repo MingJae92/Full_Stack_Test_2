c4da78c1fa75ad47528a1aeed6185ee3efe6b47a
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-12-09 21:53:09 +0000

    File path added for contributors data

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 4501;
@@ -8,8 +12,12 @@ const port = 4501;
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // Uncomment these lines if you want to use a JSON file
-// const filePath = path.join(__dirname, 'data', 'contributors.json');
-// const contributors = require(filePath);
+const filePath = new URL('file://' + path.join(__dirname, 'data', 'contributors.json')).pathname;
+
+// Use import instead of require
+import { readFileSync } from 'fs';
+const contributorsRaw = readFileSync(filePath, 'utf-8');
+const contributors = JSON.parse(contributorsRaw);
 
 // Define a simple route
 app.get('/', (req, res) => {
@@ -17,9 +25,9 @@ app.get('/', (req, res) => {
 });
 
 // Uncomment this route if you want to send JSON data
-// app.get('/contributors', (req, res) => {
-//   res.json(contributors);
-// });
+app.get('/contributors', (req, res) => {
+  res.json(contributors);
+});
 
 // Start the server
 app.listen(port, () => {
