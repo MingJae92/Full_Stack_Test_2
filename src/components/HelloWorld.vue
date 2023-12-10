c0daed647f33b230955a2b673e59d9e3b3976b05
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-12-10 01:03:00 +0000

    Vue js default boiler plate removed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,60 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div>
+    <h1>Contributors</h1>
+    <p>Total Contributors: {{ totalContributors }}</p>
+    <ul>
+      <li v-for="contributor in contributors" :key="contributor.author">
+        <h2>{{ contributor.author }}</h2>
+        <p>Total Contributions: {{ contributor.total_contributions }}</p>
+        <p>
+          Clans Contributed To:
+          <ul>
+            <li v-for="(clan, index) in contributor.clans_contributed_to" :key="index">
+              {{ clan }}
+            </li>
+          </ul>
+        </p>
+        <p>
+          Contribution Range:
+          {{ formatDate(contributor.contribution_range.start) }} -
+          {{ formatDate(contributor.contribution_range.end) }}
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { contributors } from '../data/contributor';
+
+export default {
+  data() {
+    return {
+      contributors: [],
+    };
+  },
+  computed: {
+    totalContributors() {
+      return this.contributors.length;
+    },
+  },
+  mounted() {
+    // Simulating fetching data from an API
+    // Replace this with your actual data-fetching logic
+    this.fetchContributors();
+  },
+  methods: {
+    fetchContributors() {
+      // Assuming the data is fetched from a local file
+      // Replace this with your actual fetch logic
+      this.contributors = contributors.items;
+    },
+    formatDate(date) {
+      return new Date(date).toLocaleDateString('en-GB'); // Format as DD-MM-YYYY
+    },
+  },
+};
+</script>
 
 <style scoped>
 h1 {
